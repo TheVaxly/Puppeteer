daa433c1c8e3480a78ab109bf63d952b8604b544
Marten <marten.uiboupin@gmail.com>
2023-10-03 13:05:08 +0300

    changed test.jpg name

--- Puppeteer.js
+++ Puppeteer.js
@@ -22,7 +22,7 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('http://127.0.0.1:5500/website/index.html');
   await page.setViewport({ width: 1200, height: 1400});
-  await page.screenshot({ path: 'Test-img/test.jpg' });
+  await page.screenshot({ path: 'Test-img/img-test.jpg' });
   console.log('Image taken successfully.');
   await browser.close();
 })();
